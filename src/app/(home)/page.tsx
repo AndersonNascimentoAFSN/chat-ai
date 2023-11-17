04ba81cf331dc5ff1e74cbf2e2ec0ee70c1d710c
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-11-16 21:56:26 -0300

    feat: add structure home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,20 @@
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+
 export default function Home() {
   return (
-    <main className="">
-      <h1>Hello</h1>
+    <main className="flex items-cneter justify-center">
+      <Card className="w-[400px] h-[700px]">
+        <CardHeader>
+          <CardTitle>Chart IA</CardTitle>
+          <CardDescription>Using Vercel SDK to create a chat bot.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Mensagens</p>
+        </CardContent>
+        <CardFooter>
+          <form></form>
+        </CardFooter>
+      </Card>
     </main>
   )
 }
